d13e548387fdd3d255d18c5a8a614974b3e23e04
hobpond <james.mt.leung@gmail.com>
2023-04-22 22:42:59 -0700

    Make UI easier to use

--- static/js/form_submit.js
+++ static/js/form_submit.js
@@ -2,31 +2,66 @@ function toJSONString(form) {
     const obj = {};
     const formData = new FormData(form);
     for (const [key, value] of formData) {
-        obj[key] = value;
+        if (key === 'date' && value === '') {
+            const today = new Date();
+            const dateStr = today.toISOString().slice(0, 10);
+            obj[key] = dateStr;
+        } else {
+            obj[key] = value;
+        }
     }
     return JSON.stringify(obj);
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("evaluation-form");
-
-    form.addEventListener("submit", async (event) => {
+async function submitEvaluation(event) {
+    if (event) {
         event.preventDefault();
+    }
+    
+    const form = document.getElementById("evaluation-form");
+    const formData = new FormData(form);
 
-        const json = toJSONString(form);
-        const response = await fetch("/submit-evaluation", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: json,
-        });
+    const data = {};
+    for (const [key, value] of formData.entries()) {
+        data[key] = value;
+    }
 
-        if (response.ok) {
-            alert("Thank you for submitting your evaluation.");
-            form.reset();
-        } else {
-            alert("An error occurred while submitting the evaluation.");
+    const response = await fetch("/submit_evaluation", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+
+    if (response.ok) {
+        alert("Evaluation submitted successfully!");
+        form.reset();
+    } else {
+        alert("Error submitting evaluation. Please try again.");
+    }
+}
+
+document.getElementById("evaluation-form").addEventListener("submit", submitEvaluation);
+
+
+const inputElements = document.querySelectorAll("input, textarea");
+let currentIndex = 0;
+
+inputElements.forEach((element, index) => {
+    if (index > 0) {
+        element.style.display = "none";
+    }
+
+    element.addEventListener("keydown", (event) => {
+        if (event.key === "Enter" && !event.shiftKey) {
+            event.preventDefault();
+
+            if (currentIndex < inputElements.length - 1) {
+                currentIndex++;
+                inputElements[currentIndex].style.display = "block";
+                inputElements[currentIndex].focus();
+            }
         }
     });
 });
